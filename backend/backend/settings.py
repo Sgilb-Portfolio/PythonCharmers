066e71cb9fcb47fd8256b44b616574d5f9b7f8f4
devrshah <dev@devrshah.com>
2025-02-11 01:12:08 -0500

    DB connection via settings file and migration.
    Changed the Databases dictionary in settings.py to connect with RDS database demodb. On local machine, migrate database to match the schema.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -73,13 +73,25 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+db_username = "admin"
+db_password = "b!tQOIdCiU3WzMOQ6"
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "demodb",
+        "USER": db_username,
+        "PASSWORD": db_password,
+        "HOST": "cpsc4910-team06.cobd8enwsupz.us-east-1.rds.amazonaws.com",
+        "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators

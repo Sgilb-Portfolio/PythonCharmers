4f093c522c2f7d493745d75e7db2679fcc56ebb9
Sgilb-Portfolio <sean_gilbert@icloud.com>
2025-02-13 00:23:33 -0500

    Login Page
    Does not include authentication

--- start-app/src/pages/Login.js
+++ start-app/src/pages/Login.js
@@ -1,14 +1,42 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Form, Link } from "react-router-dom";
+import { FaUser, FaLock } from "react-icons/fa";
+import '../App.css';
 
 function Login() {
     return (
-        <div>
-            <h1>Login Page</h1>
-            <Link to="/">
-                <button>Home</button>
-            </Link>
+        <div className='login'>
+            <form action="">
+                <div>
+                    <title>Login Page</title>
+                    <h1>Login Page</h1>
+                    <div className="input-box">
+                        <input type="text" placeholder='Username' required />
+                        <FaUser className="icon" />
+                    </div>
+                    <div className="input-box">
+                        <input type="password" placeholder='Password' required />
+                        <FaLock className="icon" />
+
+                    </div>
+
+                    <div className="remember-forgot">
+                        <label><input type="checkbox" />Remember me</label>
+                        <a href="#">Forgot Password</a>
+                    </div>
+
+                    <button type="submit">Login</button>
+
+                    <div className="register-link">
+                        <p>Don't have an account? <a href="/account-creation">Register</a></p>
+                    </div>
+                    <Link to="/">
+                        <button>Home</button>
+                    </Link>
+                </div>
+            </form>
         </div>
+
     );
 }
 

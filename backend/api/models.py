2caf9bfbc4948fca1cb76c47c46655f7b38fa02f
devrshah <dev@devrshah.com>
2025-02-18 17:50:11 -0500

    Update settings and About.js
    Integrate Backend with database and backend with frontend (About page)

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+  ACCOUNT_TYPES = [
+        ('Admin', 'Admin'),
+        ('Sponsor', 'Sponsor'),
+        ('Driver', 'Driver'),
+    ]
+  
+  account_id = models.AutoField(primary_key=True)
+  account_type = models.CharField(max_length=20, choices=ACCOUNT_TYPES)
+  account_username = models.CharField(max_length=150, unique=True)
+  account_password = models.CharField(max_length=150)
+
+  def __str__(self):
+    return self.account_username
+  
+
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.db import connection
 
-# Create your views here.
+def about(request):
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT DATABASE();")
+            db_name = cursor.fetchone()
+        return JsonResponse({"message": "Connected to DB", "database": db_name[0]})
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)

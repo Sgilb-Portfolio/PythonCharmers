2c44b7d29cd2f326e8cd08e6dfd843787ea45777
Sgilb-Portfolio <sean_gilbert@icloud.com>
2025-03-13 02:42:31 -0400

    Created serializers page
    Used for handling password changes when users update their profile

---
+++ backend/api/serializers.py
@@ -0,0 +1,12 @@
+#from rest_framework import serializers
+#from .models import Account
+#from django.contrib.auth.hashers import make_password
+
+#class PasswordUpdateSerializer(serializers.Serializer):
+    #old_password = serializers.CharField(write_only=True)
+    #new_password = serializers.CharField(write_only=True)
+
+    #def validate_new_password(self, value):
+       # if len(value) > 0:
+           # raise serializers.ValidationError("Password must be greater than 0 characters long")
+       # return value

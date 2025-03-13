4b38981426fd265388dc551e2a0f19f16f0f1554
Blazingle4 <cakers@clemson.edu>
2025-03-12 22:48:20 -0400

    url switch

--- start-app/src/pages/Create.js
+++ start-app/src/pages/Create.js
@@ -16,8 +16,8 @@ function Create() {
     // const onSubmit = (data) => console.log(data);
     const onSubmit = async (data) => {
         try {
-            const cognitoResponse = await fetch("http://localhost:8000/api/register-cognito/", {
-            //const cognitoResponse = await fetch("http://44.202.51.190:8000/api/register-cognito/", {
+            //const cognitoResponse = await fetch("http://localhost:8000/api/register-cognito/", {
+            const cognitoResponse = await fetch("http://44.202.51.190:8000/api/register-cognito/", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
@@ -37,8 +37,8 @@ function Create() {
             }
 
 
-            //const dbresponse = await fetch("http://44.202.51.190:8000/api/create-account/", {  // Update URL if needed
-            const dbresponse = await fetch("http://localhost:8000/api/create-account/", {
+            const dbresponse = await fetch("http://44.202.51.190:8000/api/create-account/", {  // Update URL if needed
+            //const dbresponse = await fetch("http://localhost:8000/api/create-account/", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"

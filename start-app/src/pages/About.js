537eb92755e2e5d82ccd5f816eab9ce6d22fb309
devrshah <dev@devrshah.com>
2025-03-30 16:38:28 -0400

    Update About.js

--- start-app/src/pages/About.js
+++ start-app/src/pages/About.js
@@ -17,8 +17,8 @@ function About() {
             return;
         }
 
-        //fetch("http://44.202.51.190:8000/api/about", {
-        fetch("http://localhost:8000/api/about", {
+        fetch("http://44.202.51.190:8000/api/about", {
+            // fetch("http://localhost:8000/api/about", {
             method: "GET",
             headers: {
                 "Authorization": `Bearer ${idToken}`, // Send the token here
@@ -32,8 +32,8 @@ function About() {
                 setErrorMessage("An error occurred while fetching data.");
             });
 
-        //fetch("http://44.202.51.190:8000/api/aboutdata/", {
-        fetch("http://localhost:8000/api/aboutdata/", {
+        fetch("http://44.202.51.190:8000/api/aboutdata/", {
+            // fetch("http://localhost:8000/api/aboutdata/", {
             method: "GET",
             headers: {
                 "Authorization": `Bearer ${idToken}`, // Include token in header
@@ -58,125 +58,127 @@ function About() {
             flexDirection: "column"
         }}>
             <Header />
-            
-            <div style={{ textAlign: "center", 
-        padding: "20px", 
-        }}>
-            <div>
-            <h1 style={{fontSize: "60px",color: "#333333"}}>About Page</h1>
-            </div>
 
             <div style={{
-                color: "#333333", 
-                display: "flex", 
-                justifyContent: "center", 
-                gap: "20px", 
-                marginBottom: "20px", 
-                flexWrap: "wrap"}}>
-            <div style={{
-                    backgroundColor: "#fff",
-                    borderRadius: "8px",
-                    padding: "20px",
-                    boxShadow: "0 4px 8px rgba(245, 102, 0, 1)",
-                    width: "300px",
-                    textAlign: "center"
-                }}>
-                    <p style= {{fontSize: "30px"}}><strong>Team #</strong><br></br>{aboutdata.teamNum}</p>
-            </div>
-
-            <div style={{
-                    backgroundColor: "#fff",
-                    borderRadius: "8px",
-                    padding: "20px",
-                    boxShadow: "0 4px 8px rgba(245, 102, 0, 1)",
-                    width: "300px",
-                    textAlign: "center"
-                }}>
-                    <p style={{fontSize:"30px"}}><strong>Version #</strong> <br></br>{aboutdata.versionNum}</p>
-            </div>
-
-                <div style={{
-                    backgroundColor: "#fff",
-                    borderRadius: "8px",
-                    padding: "20px",
-                    boxShadow: "0 4px 8px rgba(245, 102, 0, 1)",
-                    width: "300px",
-                    textAlign: "center"
-                }}>
-                    <p style={{fontSize:"30px"}}><strong>Release Date</strong> <br></br>{aboutdata.releaseDate}</p>
-                </div>
-                <div style={{
-                    backgroundColor: "#fff",
-                    borderRadius: "8px",
-                    padding: "20px",
-                    boxShadow: "0 4px 8px rgba(245, 102, 0, 1)",
-                    width: "1025px",
-                    textAlign: "left"
-                }}><p style={{fontSize:"20px"}}><strong>Project Name:</strong> {aboutdata.productName}</p></div>
-                <div style={{
-                    backgroundColor: "#fff",
-                    borderRadius: "8px",
-                    padding: "20px",
-                    boxShadow: "0 4px 8px rgba(245, 102, 0, 1)",
-                    width: "1025px",
-                    textAlign: "left",
-                }}><p style={{fontSize:"20px"}}><strong>Product Description:</strong> {aboutdata.productDesc}</p></div>
-            </div>
-            <Link to="/">
-                <button style={{
-                    backgroundColor: "#F56600",
-                    color: "#FFFFFF",
-                    border: "none",
-                    fontsize: "20px",
-                    borderRadius: "5px",
-                    padding: "10px 20px",
-                    cursor: "pointer",
-                    transition: "background-color 0.3 ease",
-                }}
-                onMouseEnter={(e) => e.target.style.backgroundColor = "#522D80"}
-                onMouseLeave={(e) => e.target.style.backgroundColor = "#F56600"}
-                >Home</button>
-            </Link>
-
-            <div style={{ 
-                    position: "relative",  
-                width: "80%",  
-                maxWidth: "1025px",  
-                margin: "0 auto"
-                }}>
-            <img 
-                src={myImage} 
-                alt="18 Wheeler Truck"
-                style={{ 
-                width: "100%",  
-                height: "auto",
-                borderRadius: "15px", 
-                }}/>
-                <p style={{
-                position: "absolute",
-                top: "20%",
-                left: "50%",
-                transform: "translate(-50%, -50%)",
-                fontSize: "clamp(18px, 5vw, 60px)",
-                color: "#333333",
-                padding: "10px 20px",
-                borderRadius: "8px",
                 textAlign: "center",
-                fontWeight: "bold",
-                whiteSpace: "normal",
-                maxWidth: "90%",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center"
-                }}>Python Charmers</p>
+                padding: "20px",
+            }}>
+                <div>
+                    <h1 style={{ fontSize: "60px", color: "#333333" }}>About Page</h1>
+                </div>
+
+                <div style={{
+                    color: "#333333",
+                    display: "flex",
+                    justifyContent: "center",
+                    gap: "20px",
+                    marginBottom: "20px",
+                    flexWrap: "wrap"
+                }}>
+                    <div style={{
+                        backgroundColor: "#fff",
+                        borderRadius: "8px",
+                        padding: "20px",
+                        boxShadow: "0 4px 8px rgba(245, 102, 0, 1)",
+                        width: "300px",
+                        textAlign: "center"
+                    }}>
+                        <p style={{ fontSize: "30px" }}><strong>Team #</strong><br></br>{aboutdata.teamNum}</p>
+                    </div>
+
+                    <div style={{
+                        backgroundColor: "#fff",
+                        borderRadius: "8px",
+                        padding: "20px",
+                        boxShadow: "0 4px 8px rgba(245, 102, 0, 1)",
+                        width: "300px",
+                        textAlign: "center"
+                    }}>
+                        <p style={{ fontSize: "30px" }}><strong>Version #</strong> <br></br>{aboutdata.versionNum}</p>
+                    </div>
+
+                    <div style={{
+                        backgroundColor: "#fff",
+                        borderRadius: "8px",
+                        padding: "20px",
+                        boxShadow: "0 4px 8px rgba(245, 102, 0, 1)",
+                        width: "300px",
+                        textAlign: "center"
+                    }}>
+                        <p style={{ fontSize: "30px" }}><strong>Release Date</strong> <br></br>{aboutdata.releaseDate}</p>
+                    </div>
+                    <div style={{
+                        backgroundColor: "#fff",
+                        borderRadius: "8px",
+                        padding: "20px",
+                        boxShadow: "0 4px 8px rgba(245, 102, 0, 1)",
+                        width: "1025px",
+                        textAlign: "left"
+                    }}><p style={{ fontSize: "20px" }}><strong>Project Name:</strong> {aboutdata.productName}</p></div>
+                    <div style={{
+                        backgroundColor: "#fff",
+                        borderRadius: "8px",
+                        padding: "20px",
+                        boxShadow: "0 4px 8px rgba(245, 102, 0, 1)",
+                        width: "1025px",
+                        textAlign: "left",
+                    }}><p style={{ fontSize: "20px" }}><strong>Product Description:</strong> {aboutdata.productDesc}</p></div>
+                </div>
+                <Link to="/">
+                    <button style={{
+                        backgroundColor: "#F56600",
+                        color: "#FFFFFF",
+                        border: "none",
+                        fontsize: "20px",
+                        borderRadius: "5px",
+                        padding: "10px 20px",
+                        cursor: "pointer",
+                        transition: "background-color 0.3 ease",
+                    }}
+                        onMouseEnter={(e) => e.target.style.backgroundColor = "#522D80"}
+                        onMouseLeave={(e) => e.target.style.backgroundColor = "#F56600"}
+                    >Home</button>
+                </Link>
+
+                <div style={{
+                    position: "relative",
+                    width: "80%",
+                    maxWidth: "1025px",
+                    margin: "0 auto"
+                }}>
+                    <img
+                        src={myImage}
+                        alt="18 Wheeler Truck"
+                        style={{
+                            width: "100%",
+                            height: "auto",
+                            borderRadius: "15px",
+                        }} />
+                    <p style={{
+                        position: "absolute",
+                        top: "20%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        fontSize: "clamp(18px, 5vw, 60px)",
+                        color: "#333333",
+                        padding: "10px 20px",
+                        borderRadius: "8px",
+                        textAlign: "center",
+                        fontWeight: "bold",
+                        whiteSpace: "normal",
+                        maxWidth: "90%",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center"
+                    }}>Python Charmers</p>
+                </div>
+                {data ? (
+                    <p>{data.message} - Connected to: {data.database}</p>
+                ) : (
+                    <p>Loading....</p>
+                )}
             </div>
-            {data ? (
-                <p>{data.message} - Connected to: {data.database}</p>
-            ) : (
-                <p>Loading....</p>
-            )}
-        </div>
-            
+
             <Footer />
         </div>
     );
